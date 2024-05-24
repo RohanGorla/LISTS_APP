80000f568fc977c2f29df59b6cdd283c2a58b6f6
Rohan <rohangorla07@gmail.com>
2024-05-24 06:24:39 +0530

    add basic styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,41 +12,53 @@ function App() {
         </div>
         <div className="todo-container">
           <div className="todo-list-container">
-            <div>
+            <div className="list-header">
               <h2>MY TODO-LISTS</h2>
             </div>
             <div className="list-input">
-              <input></input>
-              <button>+</button>
+              <input id="list-name" placeholder="LIST NAME..."></input>
+              <button>ADD</button>
             </div>
             <div className="todo-lists">
               <div className="list-card">
                 <h3 className="list-name">LIST NAME</h3>
-                <button className="create-list">Select</button>
-                <button className="delete-list">DELETE</button>
+                <div className="list-options">
+                  <button className="create-list">Select</button>
+                  <button className="delete-list">DELETE</button>
+                </div>
               </div>
             </div>
           </div>
           <div className="todos-container">
             <div className="todos-header">
               <h2>TODO-LIST NAME</h2>
-              <div className="count">
+              {/* <div className="count">
                 <p>1 todo left</p>
-              </div>
+              </div> */}
             </div>
-            <div className="todo-input">
-              <input></input>
-              <button>+</button>
+            <div className="todos-input">
+              {/* <div className="todos-title">
+                <label>Title</label>
+                <input></input>
+              </div> */}
+              <div className="todos-content">
+                {/* <label>Content</label> */}
+                <input placeholder="New todo item..."></input>
+                <button>ADD</button>
+              </div>
             </div>
             <div className="todos">
               <div className="todo-card">
-                <h3 className="todo-title">THIS IS A TODO</h3>
-                <p className="todo-content">THIS IS THE TODO CONTENT</p>
-                <button className="button-done">DONE</button>
+                {/* <h3 className="todo-title">THIS IS A TODO</h3> */}
+                <div className="todo-info">
+                  <p>THIS IS THE TODO CONTENT</p>
+                  <button className="btn-done">DONE</button>
+                </div>
               </div>
             </div>
-            <div className="todo-options">
-              <button className="remove-finished">REMOVE FINISHED</button>
+            <div className="todos-options">
+              <button className="remove-finished">REMOVE FINISHED TODOS</button>
+              <button className="remove-all">REMOVE ALL TODOS</button>
             </div>
           </div>
         </div>
