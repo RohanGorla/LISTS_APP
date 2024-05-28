c02ed1806b6e24a1eb4732fc01290bc40fbb0b5b
Rohan <rohangorla07@gmail.com>
2024-05-28 23:05:21 +0530

    add functionality to perform CRUD operations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,22 +6,118 @@ import "./styles/App.css";
 
 function App() {
   const [lists, setLists] = useState([]);
-  const [tods, steTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
+  const [listInput, setListInput] = useState("");
   const [currentList, setCurrentList] = useState("");
 
   useEffect(() => {
-    async function getData() {
-      await axios
-        .get(`${import.meta.env.VITE_BASE_URL}/`)
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-    getData();
+    getLists();
   }, []);
+
+  async function getLists() {
+    await axios
+      .get(`${import.meta.env.VITE_BASE_URL}/`)
+      .then((response) => {
+        setLists(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  async function addList() {
+    await axios
+      .post(`${import.meta.env.VITE_BASE_URL}/addlist`, { listname: listInput })
+      .then((response) => {
+        getLists();
+      })
+      .catch((err) => console.log(err));
+    setListInput("");
+  }
+
+  async function selectList(listname) {
+    await axios
+      .post(`${import.meta.env.VITE_BASE_URL}/getlist`, { listname: listname })
+      .then((response) => {
+        setTodos(response.data);
+        setCurrentList(listname);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  async function deleteList(listname) {
+    await axios
+      .delete(`${import.meta.env.VITE_BASE_URL}/deletelist`, {
+        data: { listname },
+      })
+      .then((response) => {
+        getLists();
+        if (currentList == listname) {
+          selectList('');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  async function addTodo() {
+    await axios
+      .post(`${import.meta.env.VITE_BASE_URL}/addtodo`, {
+        listname: currentList,
+        todo: todoInput,
+      })
+      .then((response) => {
+        selectList(currentList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setTodoInput("");
+  }
+
+  async function setDone(id, todo_done) {
+    await axios
+      .post(`${import.meta.env.VITE_BASE_URL}/setdone`, {
+        id: id,
+        done: todo_done == "yes" ? "no" : "yes",
+      })
+      .then((response) => {
+        selectList(currentList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  async function deleteDone(listname) {
+    await axios
+      .delete(`${import.meta.env.VITE_BASE_URL}/deletedone`, {
+        data: { listname },
+      })
+      .then((response) => {
+        selectList(currentList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  async function deleteAll(listname) {
+    await axios
+      .delete(`${import.meta.env.VITE_BASE_URL}/deleteall`, {
+        data: { listname },
+      })
+      .then((response) => {
+        selectList(currentList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   return (
     <>
@@ -35,22 +131,53 @@ function App() {
               <h2>MY TODO-LISTS</h2>
             </div>
             <div className="list-input">
-              <input id="list-name" placeholder="LIST NAME..."></input>
-              <button>ADD</button>
+              <input
+                id="list-name"
+                placeholder="LIST NAME..."
+                onChange={(e) => {
+                  setListInput(e.target.value);
+                }}
+                value={listInput}
+              ></input>
+              <button
+                onClick={() => {
+                  addList();
+                }}
+              >
+                ADD
+              </button>
             </div>
             <div className="todo-lists">
-              <div className="list-card">
-                <h3 className="list-name">LIST NAME</h3>
-                <div className="list-options">
-                  <button className="create-list">Select</button>
-                  <button className="delete-list">DELETE</button>
-                </div>
-              </div>
+              {lists.map((list) => {
+                return (
+                  <div className="list-card" key={list.id}>
+                    <h3 className="list-name">{list.listname}</h3>
+                    <div className="list-options">
+                      <button
+                        className="select-list"
+                        onClick={() => {
+                          selectList(list.listname);
+                        }}
+                      >
+                        Select
+                      </button>
+                      <button
+                        className="delete-list"
+                        onClick={() => {
+                          deleteList(list.listname);
+                        }}
+                      >
+                        DELETE
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
           <div className="todos-container">
             <div className="todos-header">
-              <h2>TODO-LIST NAME</h2>
+              <h2>{currentList}</h2>
               {/* <div className="count">
                 <p>1 todo left</p>
               </div> */}
@@ -62,22 +189,61 @@ function App() {
               </div> */}
               <div className="todos-content">
                 {/* <label>Content</label> */}
-                <input placeholder="New todo item..."></input>
-                <button>ADD</button>
+                <input
+                  placeholder="New todo item..."
+                  onChange={(e) => {
+                    setTodoInput(e.target.value);
+                  }}
+                  value={todoInput}
+                ></input>
+                <button
+                  onClick={() => {
+                    addTodo();
+                  }}
+                >
+                  ADD
+                </button>
               </div>
             </div>
             <div className="todos">
-              <div className="todo-card">
-                {/* <h3 className="todo-title">THIS IS A TODO</h3> */}
-                <div className="todo-info">
-                  <p>THIS IS THE TODO CONTENT</p>
-                  <button className="btn-done">DONE</button>
-                </div>
-              </div>
+              {todos.map((todo) => {
+                return (
+                  <div className="todo-card" key={todo.id}>
+                    {/* <h3 className="todo-title">THIS IS A TODO</h3> */}
+                    <div className="todo-info">
+                      <p className={todo.done == "yes" ? "todo done" : "todo"}>
+                        {todo.todo}
+                      </p>
+                      <button
+                        className="btn-done"
+                        onClick={() => {
+                          setDone(todo.id, todo.done);
+                        }}
+                      >
+                        DONE
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
             <div className="todos-options">
-              <button className="remove-finished">REMOVE FINISHED TODOS</button>
-              <button className="remove-all">REMOVE ALL TODOS</button>
+              <button
+                className="remove-finished"
+                onClick={() => {
+                  deleteDone(currentList);
+                }}
+              >
+                REMOVE FINISHED TODOS
+              </button>
+              <button
+                className="remove-all"
+                onClick={() => {
+                  deleteAll(currentList);
+                }}
+              >
+                REMOVE ALL TODOS
+              </button>
             </div>
           </div>
         </div>
--- server/server.js
+++ server/server.js
@@ -23,7 +23,57 @@ db.connect((err) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello...");
+  db.query("select * from lists", (err, data) => {
+    if (err) return res.send(err);
+    return res.send(data);
+  });
+});
+
+app.post("/addlist", (req, res) => {
+  let values = [[req.body.listname]];
+  db.query("insert into lists (listname) values ?", [values]);
+  res.send("done");
+});
+
+app.post("/getlist", (req, res) => {
+  db.query(
+    "select * from todos where listname = ?",
+    [req.body.listname],
+    (err, data) => {
+      if (err) return res.send(err);
+      return res.send(data);
+    }
+  );
+});
+
+app.delete("/deletelist", (req, res) => {
+  db.query("delete from lists where listname = ?", [req.body.listname]);
+  db.query("delete from todos where listname = ?", [req.body.listname]);
+  res.send("deleted");
+});
+
+app.post("/addtodo", (req, res) => {
+  let values = [[req.body.listname, req.body.todo]];
+  db.query("insert into todos (listname, todo) values ?", [values]);
+  res.send("added");
+});
+
+app.post("/setdone", (req, res) => {
+  db.query("update todos set ? where id = ?", [
+    { done: req.body.done },
+    req.body.id,
+  ]);
+  res.send("done");
+});
+
+app.delete("/deletedone", (req, res)=>{
+    db.query("delete from todos where listname = ? AND done = 'yes'", [req.body.listname]);
+    res.send("deleted");
+})
+
+app.delete("/deleteall", (req, res) => {
+  db.query("delete from todos where listname = ?", [req.body.listname]);
+  res.send("deleted");
 });
 
 app.listen(PORT, () => {
